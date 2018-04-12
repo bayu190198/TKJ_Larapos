cf60c1aacf1afa5492eff763f4ce6ea201d93eff
Bayu Saputro <bayu190198@users.noreply.github.com>
2018-04-12 16:29:56 +0700

    Laravel

---
+++ TKJ Larapos/resources/views/category/index.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+@section('content')
+<div class="table-responsive">
+	<table class="table	table-striped">
+		<tr>
+			<th>No</th>
+			<th>Category Name</th>
+			<th>Action</th>
+		</tr>
+	</table>
+</div>
+@endsection
